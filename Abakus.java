95a28791bd310e4917ec93c234a6073e926dda98
Ahmet İhtiyar <92849570+ihtiyar07@users.noreply.github.com>
2024-03-01 00:40:17 +0300

    Add files via upload

---
+++ Abakus.java
@@ -0,0 +1,16 @@
+package A;
+
+public class Abakus {
+    public static void main(String []args){
+        /*for(int i=1; i<100; i++){
+            if(i==100){
+                i-=2;
+                System.out.print("\n");}
+            System.out.print(i+" ");
+
+            if(i==0)
+                break;
+        }*/
+        System.out.println((int)(Math.random()* 90));
+    }
+}
